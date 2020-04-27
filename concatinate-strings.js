013619e6dc132b78f856b8c885df4cd7b92892c9
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-04-27 17:21:49 +0530

    Retrieve the last character from a string.

--- concatinate-strings.js
+++ concatinate-strings.js
@@ -1,7 +1,15 @@
 var firstName = "Jason "
 var lastName = "Chase"
 
-console.log(firstName += lastName);
+console.log(firstName += lastName)
 
-console.log("Jason " + "Chase");
+console.log("Jason " + "Chase")
+
+console.log(firstName[0])
+console.log(firstName[1])
+console.log(firstName[2])
+console.log(firstName[3])
+console.log(firstName[4])
+
+console.log("Last character of " + firstName + " is " + firstName[firstName.length - 1])
 
