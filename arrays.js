d4fa9cf3375457a8604fae80f27d36b5b5caf927
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-04-27 18:39:37 +0530

    Push and pop elements to and from the arrays.

--- arrays.js
+++ arrays.js
@@ -5,21 +5,28 @@ console.log(twoDimenArray[0][1])
 
 twoDimenArray.forEach(outer => {
     outer.forEach(inner => {
-        console.log(inner);
+        console.log(inner)
     })
 });
 
 for (const i in twoDimenArray) {
     var plant = twoDimenArray[i]
     for (const j in plant) {
-        console.log(plant[j]);
+        console.log(plant[j])
     }
 }
 
 for (var i = 0; i < twoDimenArray.length; i++) {
     var plant = twoDimenArray[i]
     for (var j = 0; j < plant.length; j++) {
-        console.log(plant[j]);
+        console.log(plant[j])
         
     }
 }
+
+console.log("Before push, the array is : " + twoDimenArray)
+twoDimenArray.push(["Snake Plant", 6])
+console.log("After push, the array is : " + twoDimenArray)
+// Can only remove the last item.
+twoDimenArray.pop()
+console.log("After pop, the array is : " + twoDimenArray)
