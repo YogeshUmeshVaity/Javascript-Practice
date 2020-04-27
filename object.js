10b24f81069f54635324180f8b48dfebef6ba99f
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-04-28 00:20:30 +0530

    Access properties of the object using brackets.

--- object.js
+++ object.js
@@ -1,7 +1,8 @@
 const plant = {
-    name: "Rubber Plant",
+    "plant name": "Rubber Plant",
     sizeInches: 24,
     color: "Green"
 }
 
 console.log("Plants color is " + plant.color)
+console.log("Access property using brackets " + plant["plant name"]);
